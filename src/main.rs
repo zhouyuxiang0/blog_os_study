c48f1b3a092367a31cb4b8442c4cd00184a863ca
1521350289@qq.com <1521350289@qq.com>
2020-12-20 20:10:45 +0800

    feat: 增加入口函数 错误处理函数 禁用std及运行时入口函数

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]    // 禁用名称重写 编译器将输出_start函数
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
