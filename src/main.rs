2fdb5ac4582115b80c00ec6e0ab7c59aadc56227
1521350289@qq.com <1521350289@qq.com>
2021-01-02 23:36:22 +0800

    print

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
+
+mod vga_buffer;
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
+static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]    // 禁用名称重写 编译器将输出_start函数
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
     loop {}
 }
